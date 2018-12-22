e463388e280f8f36ba46ae2c5089b0520c403f55
Byron Dunkley <KamonLeigh@users.noreply.github.com>
2018-12-22 01:34:13 +0000

    added html for navbar started building app.js

--- assignment-3/public/app.js
+++ assignment-3/public/app.js
@@ -7,5 +7,22 @@
 
  const app = {};
 
+ // AJAX client ( for RESTful API)
+ app.client = {}
+
+ // Interface for making API calls
+ app.client.request = (headers, path, method, queryStringObject, payload, callback) => {
+
+    // Set defaults
+    headers = typeof(headers) === 'object' && headers !== null ? headers : null;
+    path = typeof(path) === 'string' ? path : '/';
+    method = typeof(method) === 'string' && ['POST', 'GET', 'DELETE', 'PUT'].indexOf(method.toUpperCase()) > -1 ? method : 'GET';
+    queryStringObject = typeof(queryStringObject) === 'object' && queryStringObject !== null ? queryStringObject : {};
+    payload = typeof(payload) === 'object' && payload !== null ? payload : {};
+    callback = typeof(callback) == 'function' ? callback : false;
+
+    // For each query string parametter sent, add it to the path
+ }
+
 
 
