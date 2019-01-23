5679f388bcda33c0dc9da78ad92e9ca4b54ef8c9
Byron Dunkley <KamonLeigh@users.noreply.github.com>
2019-01-23 17:06:00 +0000

    added emit event function and listener

--- assignment-4/lib/cli.js
+++ assignment-4/lib/cli.js
@@ -15,8 +15,116 @@
  const cli = {}
 
 
-// Init Script
+// Input processor
+cli.processInput = (str) => {
+    str = typeof(str) == 'string' && str.trim().lengh > 0 ? str.trim() : false;
 
+    // Only process the string if user wrote something
+    if(str){
+        
+        // Codify the list of commands
+        const uniqueInputs = [
+            'man',
+            'help',
+            'menu',
+            'list users',
+            'recent orders',
+            'orders',
+            'specifc user',
+            'exit',
+            'stats',
+            'sign up',
+            'sign in',
+
+        ];
+
+        // Loop through the arry when match is found emit an event
+        let matchFound = false;
+        let counter = 0;
+
+        uniqueInputs.some( input => {
+
+            if(input.includes(str.toLowerCase())) {
+                matchFound = true;
+
+                // Emit the input along with the string 
+                e.emit(input, str);
+
+                // Add try to break out of the loop
+                return true;
+            }
+
+        });
+
+        if(!matchFound){
+            console.log('Sorry please try again');
+        }
+
+    }
+}
+
+
+
+
+
+// Create Event Listener for cli input 
+e.on('man', (str) => {
+    cli.responders.help();
+})
+
+e.on('help', (str) => {
+    cli.responders.help();
+});
+
+e.on('menu', (str) => {
+    cli.responders.menu();
+});
+
+e.on('list users', (str) => {
+    cli.responders.listUsers();
+});
+
+e.on('recent orders', (str) => {
+    cli.responders.recentOrders();
+});
+
+e.on('orders', (str) => {
+    cli.responders.orders();
+});
+
+e.on('specific user', (str) => {
+    cli.responders.specificUser(str);
+});
+
+e.on('exit', (str) => {
+    cli.responders.exit();
+});
+
+e.on('stats', () => {
+    cli.responders.stats();
+});
+
+e.on('sign up', () => {
+    cli.responders.signUp();
+});
+
+e.on('sign in', () => {
+    cli.responders.signIn()
+});
+
+
+
+
+// Responce handlers
+cli.responders = {};
+
+
+
+cli.responders.exit = () => {
+    process.exit(0);
+};
+
+// Init Script
 cli.init = () => {
     // Send a message to the console in dark blue
     console.log('\x1b[34m%s\x1b[0m', "The CLI is running ");
@@ -28,14 +136,26 @@ cli.init = () => {
         prompt:''
     });
 
+    // Create an intial prompt
+    _interface.prompt();
+
+    // Handle each line of input separately
+    _interface.on('line',(str) => {
+        // send to the input processor
+        cli.processInput(str);
+
+        // Restart the prompt
+        _interface.prompt();
+
+
+    });
+
+    // If the user stops the cli kill the asscoiated process
+    _interface.on('close', () => {
+        process.exit(0)
+    });
+
 }
-
-
-
-
-
-
-
 
 
 // Export the module 
